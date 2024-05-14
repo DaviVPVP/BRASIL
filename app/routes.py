1dd9a947d3502498f3f70de8379333ea8a393c7a
DAVI VITAL DO PRADO VICENTE PEREIRA <davi.vpvpereira@senacsp.edu.br>
2024-05-13 22:11:15 -0300

    first commit

--- app/routes.py
+++ app/routes.py
@@ -5,15 +5,31 @@ from flask import request
 @app.route('/') #Configurando um rota web
 @app.route('/index')
 def index():
-    return render_template('index.html', titulo="Página Inicial", nome="Davids")
+    return render_template('index.html', titulo="Início", nome="Davids")
 
 @app.route('/contato')
 def contato():
-    return render_template('contato.html', titulo="Contato", nome="Davids")
+    return render_template('contato.html', titulo="Dani Alves", nome="Davids")
 
-@app.route('/Dani Alves')
+@app.route('/tres')
 def tres():
-    return render_template('tres.html', titulo="Dani Alves", nome="Davids")
+    return render_template('tres.html', titulo="Thiago Silva", nome="Davids")
+
+@app.route('/four')
+def four():
+    return render_template('four.html', titulo="Marcelo", nome="Davids")
+
+@app.route('/cassio')
+def cassio():
+    return render_template('cassio.html', titulo="Cassio", nome="Davids")
+
+@app.route('/case')
+def case():
+    return render_template('case.html', titulo="Casemiro", nome="Davids")
+
+
+
+
 
 
 
